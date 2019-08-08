58309bf68060ada45bf22a2650714fa24f85dca4
Tomas Eglinskas <tomase@wix.com>
2019-08-09 01:03:05 +0300

    Clean up favicon with homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Tomas Eglinskas`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `The simple place where I blog about the things I like.`,
+    author: `Tomas Eglinskas`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/circle-256.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,60 @@
 import React from "react"
-import styled from 'styled-components'
+import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Post from '../components/Post'
 import SEO from "../components/seo"
-import Label from "../components/Label";
 
-const LabelContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 80px;
+const Title = styled.h3`
+  font-size: 36px;
+  font-weight: 600;
+  margin: 20px 0px;
 `
 
-const PostContainer = styled.div`
+const Text = styled.p`
+  font-size: 16px;
+  line-spacing: 25;
+`
+
+const Container = styled.div`
   position: absolute;
-  top: 250px;
+  top: 150px;
   right: 0;
   bottom: 0;
   left: 0;
   padding: 20px;
   margin: auto;
-  text-align:center;
-`
-
-const Text = styled.span`
-  max-width: 960px;
+  max-width: 460px;
   font-family: "SF Compact Display";
-  font-size: 18px;
-  font-weight: 600;
-  margin: 10px 0px;
-
 `
 
-const IndexPage = () => (
+const About = () => (
   <Layout>
-    <SEO title="Home" />
-    <LabelContainer>
-      <Label name="AI"/>
-      {/* <Label name="Leet Code" bgColor="#FDC536" hoverColor="#FFB906"/> */}
-
-    </LabelContainer>
-    <PostContainer>
-      {/* <Post /> */}
-      <Text>🚧 In the works 🚧</Text>
-    </PostContainer>
+    <SEO title="About" />
+    <Container>
+      <Title>About</Title>
+      <Text>
+        <p>👋 Hey, I'm Tomas </p>
+        <p>
+          I'm a software engineer @ Wix, who's searching the world trying to
+          understand myself and others.
+        </p>
+        <p>
+          At the moment I'm being proactive in the startup field, participating
+          at incubators, hackatons and creating projects for myself. Similary
+          the idea of this blog came along - wanting to create something for
+          myself to remember, and maybe others to scroll by.
+        </p>
+        <p>
+          My main virtue is people, not technology, as I trully believe that
+          people are everything.
+        </p>
+        <p>
+          Therefore don't be afraid to say hi or ask questions. I'm also freely
+          reachable at <a href="https://www.linkedin.com/in/tomas-eglinskas">@LinkedIn</a> and  <a href="mailto:tomas.eglinskas@gmail.com">@Mail</a>
+        </p>
+      </Text>
+    </Container>
   </Layout>
 )
 
-export default IndexPage
+export default About
