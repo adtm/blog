d58eec68d113910f689970646cca64b7761f64b8
Tomas Eglinskas <tomase@wix.com>
2019-08-10 02:48:22 +0300

    C

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,38 +1,38 @@
-// import React from 'react'
-// import styled from 'styled-components'
-// import arrowLeft from '../icons/arrow-left.svg'
+import React from 'react'
+import styled from 'styled-components'
+import arrowLeft from '../icons/arrow-left.svg'
 
-// import Layout from '../components/layout';
-// import SEO from '../components/seo';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-// const Title = styled.h3`
-//     font-size: 32px;
-//     font-weight: 600;
-//     margin: 20px 0px;
-// `
+const Title = styled.h3`
+    font-size: 32px;
+    font-weight: 600;
+    margin: 20px 0px;
+`
 
-// const Text = styled.p`
-//     font-size: 14px;
-//     line-spacing: 25;
-// `
+const Text = styled.p`
+    font-size: 14px;
+    line-spacing: 25;
+`
 
-// const Container = styled.div`
-//     margin: auto;
-//     margin-top: 150px;
-//     max-width: 450px;
-// `
+const Container = styled.div`
+    margin: auto;
+    margin-top: 150px;
+    max-width: 450px;
+`
 
-// const Post = () => (
-//     <Layout>
-//         <SEO title="Post" />
-//         <Container>
-//          <img src={arrowLeft}/>
-//             <Title>Blog Post</Title>
-//             <Text>
-//                 Some text
-//             </Text>
-//         </Container>
-//     </Layout>
-// )
+const Post = () => (
+    <Layout>
+        <SEO title="Post" />
+        <Container>
+         <img src={arrowLeft}/>
+            <Title>Blog Post</Title>
+            <Text>
+                Some text
+            </Text>
+        </Container>
+    </Layout>
+)
 
-// export default Post;
+export default Post;
--- src/pages/random.js
+++ src/pages/random.js
@@ -1,50 +1,50 @@
-// import React from "react"
-// import styled from 'styled-components'
+import React from "react"
+import styled from 'styled-components'
 
-// import Layout from "../components/layout"
-// import Post from '../components/Post'
-// import SEO from "../components/seo"
-// import Label from "../components/Label";
+import Layout from "../components/layout"
+import Post from '../components/Post'
+import SEO from "../components/seo"
+import Label from "../components/Label";
 
-// const LabelContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   margin-top: 80px;
-// `
+const LabelContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 80px;
+`
 
-// const PostContainer = styled.div`
-//   position: absolute;
-//   top: 250px;
-//   right: 0;
-//   bottom: 0;
-//   left: 0;
-//   padding: 20px;
-//   margin: auto;
-//   text-align:center;
-// `
+const PostContainer = styled.div`
+  position: absolute;
+  top: 250px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 20px;
+  margin: auto;
+  text-align:center;
+`
 
-// const Text = styled.span`
-//   max-width: 960px;
-//   font-family: "SF Compact Display";
-//   font-size: 18px;
-//   font-weight: 600;
-//   margin: 10px 0px;
+const Text = styled.span`
+  max-width: 960px;
+  font-family: "SF Compact Display";
+  font-size: 18px;
+  font-weight: 600;
+  margin: 10px 0px;
 
-// `
+`
 
-// const IndexPage = () => (
-//   <Layout>
-//     <SEO title="Home" />
-//     <LabelContainer>
-//       <Label name="AI"/>
-//       {/* <Label name="Leet Code" bgColor="#FDC536" hoverColor="#FFB906"/> */}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <LabelContainer>
+      <Label name="AI"/>
+      {/* <Label name="Leet Code" bgColor="#FDC536" hoverColor="#FFB906"/> */}
 
-//     </LabelContainer>
-//     <PostContainer>
-//       {/* <Post /> */}
-//       <Text>🚧 In the works 🚧</Text>
-//     </PostContainer>
-//   </Layout>
-// )
+    </LabelContainer>
+    <PostContainer>
+      {/* <Post /> */}
+      <Text>🚧 In the works 🚧</Text>
+    </PostContainer>
+  </Layout>
+)
 
-// export default IndexPage
+export default IndexPage
