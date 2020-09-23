a73a2b1c6ece05528a17f645b5e3a7577666a972
Tomas Eglinskas <tomase@wix.com>
2020-09-23 18:34:38 +0300

    add sc

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         precachePages: [`/about`, `/`],
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-layout`,
