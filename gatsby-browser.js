df333dfba11d402999ffbb7685b95352899f44ee
Tomas Eglinskas <tomase@wix.com>
2020-10-04 12:52:06 +0300

    chore: more helper fns

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core"
-import theme from "./src/gatsby-plugin-chakra-ui/theme"
+import theme from "./src/gatsy-plugin-chakra-ui/theme"
 
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,9 @@
+import { ColorModeScript } from "@chakra-ui/core";
+
 export { wrapRootElement } from "./gatsby-browser"
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+    setPreBodyComponents([
+      <ColorModeScript initialColorMode="light" key="chakra-ui-no-flash" />,
+    ])
+  }
